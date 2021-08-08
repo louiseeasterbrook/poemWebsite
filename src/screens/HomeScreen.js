dbeaf22df2da7bbf6174d44d3f3a0b6c21fc561b
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-08-09 00:02:08 +1000

    create + style home screen layout

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,21 @@
 import "./homeScreen.css";
 
+//components
+import PoemItem from "../components/PoemItem";
+
 const HomeScreen = () => {
-  return <div className="homeScreen">Home screen</div>;
+  return (
+    <div className="homeScreen">
+      <div className="poemContainer">
+        <PoemItem />
+        <PoemItem />
+        <PoemItem />
+        <PoemItem />
+        <PoemItem />
+        <PoemItem />
+      </div>
+    </div>
+  );
 };
 
 export default HomeScreen;
