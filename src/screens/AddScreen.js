fe2c369d9ebfc225954b1dd6f096769c365b1c03
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-08-20 14:15:21 +1000

    add Titles to add screen

--- src/screens/AddScreen.js
+++ src/screens/AddScreen.js
@@ -1,7 +1,16 @@
 import "./addScreen.css";
 
+//components
+import PoemForm from "../components/PoemForm";
+
 const AddScreen = () => {
-  return <div className="addScreen">add Screen</div>;
+  return (
+    <div className="addScreen">
+      <h2>Title</h2>
+      <p>Enter your peom details</p>
+      <PoemForm />
+    </div>
+  );
 };
 
 export default AddScreen;
