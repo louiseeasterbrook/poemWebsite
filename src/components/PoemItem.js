bc1ec20438e77e451c1a4b097ed6d01019b51e24
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-08-20 14:16:16 +1000

    add link to poem item

--- src/components/PoemItem.js
+++ src/components/PoemItem.js
@@ -1,23 +1,26 @@
 import "./poemItem.css";
 import userPic from "../user.png";
+import { Link } from "react-router-dom";
 // import { Link } from "react-router-dom";
 
-const PoemItem = () => {
+const PoemItem = (props) => {
   return (
-    <div className="poemItemContainer">
-      <div className="poemContent">
-        <h1>Poem Title</h1>
-        <p>
-          this is the 1poem
-          <br /> line second <br />
-          line third line....
-        </p>
-      </div>
+    <div onClick={props.toPoem} className="poemItemContainer">
+      <Link to="/poem">
+        <div className="poemContent">
+          <h1>Poem Title</h1>
+          <p>
+            this is the 1poem
+            <br /> line second <br />
+            line third line....
+          </p>
+        </div>
 
-      <div className="authorSection">
-        <img src={userPic} alt="author" className="authorImg" />
-        <p className="authorName">Louise Easterbrok</p>
-      </div>
+        <div className="authorSection">
+          <img src={userPic} alt="author" className="authorImg" />
+          <p className="authorName">Louise Easterbrok</p>
+        </div>
+      </Link>
     </div>
   );
 };
