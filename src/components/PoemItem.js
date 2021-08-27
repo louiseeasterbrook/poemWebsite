84ebab170c5cd5d4be7495a2b7403560d7ae5868
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-08-27 13:05:45 +1000

    add function to extract first three lines of poem

--- src/components/PoemItem.js
+++ src/components/PoemItem.js
@@ -4,13 +4,31 @@ import { Link } from "react-router-dom";
 // import { Link } from "react-router-dom";
 
 const PoemItem = (props) => {
-  console.log(props.text);
+  var firstLine = "";
+  var secondLine = "";
+  var thirdLine = "";
+  const getLines = () => {
+    if (props.poemText.split("\n")[0]) {
+      firstLine = props.poemText.split("\n")[0];
+    }
+    if (props.poemText.split("\n")[1]) {
+      secondLine = props.poemText.split("\n")[1];
+    }
+    if (props.poemText.split("\n")[2]) {
+      thirdLine = props.poemText.split("\n")[2];
+    }
+  };
+
+  getLines();
+
   return (
     <div onClick={props.toPoem} className="poemItemContainer">
       <Link to={`/poem/${props.poemId}`}>
         <div className="poemContent">
           <h1>{props.poemTitle}</h1>
-          <p>{props.poemText}</p>
+          <p>{firstLine}</p>
+          <p>{secondLine}</p>
+          <p>{thirdLine}</p>
         </div>
 
         <div className="authorSection">
