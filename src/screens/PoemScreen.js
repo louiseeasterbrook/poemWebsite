b6b165bd15b06a8964c72ac4e4cfda1ca8b838a9
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-09-04 14:52:34 +1000

    render error if no poem is fetched

--- src/screens/PoemScreen.js
+++ src/screens/PoemScreen.js
@@ -7,18 +7,25 @@ import ReactMarkdown from "react-markdown";
 import { Redirect } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import Notification from "../components/Notification";
 
 const PoemScreen = ({ poems, setPoems }) => {
   let { id } = useParams();
   const [poem, setPoem] = useState([]);
   let updatedPoem = {};
   let newNum = 0;
+  const [error, setError] = useState("");
 
   // GET DATA FROM SERVER
   useEffect(() => {
-    axios.get(`http://localhost:3001/api/poems/${id}`).then((response) => {
-      setPoem(response.data);
-    });
+    axios
+      .get(`http://localhost:3001/api/poems/${id}`)
+      .then((response) => {
+        setPoem(response.data);
+      })
+      .catch((error) => {
+        setError("Poem not Found");
+      });
   }, []);
 
   const updateVotes = () => {
@@ -39,21 +46,33 @@ const PoemScreen = ({ poems, setPoems }) => {
       });
   };
 
-  return (
-    <div className="poemScreen">
-      <div className="poemScreenContent">
-        <h2 className="showTitle">{poem.title}</h2>
-        <div className="authorDisplay">
-          <FontAwesomeIcon icon={faUserCircle} className="userIcon" />
-          <p className="authorName">{poem.author}</p>
+  //render error if error has occured
+  if (Boolean(error)) {
+    return (
+      <div className="poemScreen">
+        <div className="poemScreenContent">
+          <Notification message={error} show={true} />
         </div>
-        <div className="poemHolder">
-          <ReactMarkdown className="poemScreenText" children={poem.text} />
-        </div>
-        <Votes votes={poem.votes} voteAdd={updateVotes} />
       </div>
-    </div>
-  );
+    );
+  } else {
+    //if no error render poem screen
+    return (
+      <div className="poemScreen">
+        <div className="poemScreenContent">
+          <h2 className="showTitle">{poem.title}</h2>
+          <div className="authorDisplay">
+            <FontAwesomeIcon icon={faUserCircle} className="userIcon" />
+            <p className="authorName">{poem.author}</p>
+          </div>
+          <div className="poemHolder">
+            <ReactMarkdown className="poemScreenText" children={poem.text} />
+          </div>
+          <Votes votes={poem.votes} voteAdd={updateVotes} />
+        </div>
+      </div>
+    );
+  }
 };
 
 export default PoemScreen;
