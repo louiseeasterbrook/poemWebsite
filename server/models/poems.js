91f2080f1e32c89bdbdf57f858836dd43fd9e090
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-09-14 22:09:40 +1000

    remove redundant code

--- server/models/poems.js
+++ server/models/poems.js
@@ -9,8 +9,6 @@ mongoose
   .connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useFindAndModify: false,
-    // useCreateIndex: true,
   })
   .then((result) => {
     console.log("connected to MongoDB");
