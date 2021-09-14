23f56fb8c6e56828a6183c24d96dfe8c6b3ff337
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-09-14 22:07:03 +1000

    added post request to update data

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -3,18 +3,12 @@ const Poem = require("../models/poems");
 const logger = require("../utils/logger");
 const crypto = require("crypto");
 
+//GET ALL
 poemsRouter.get("/", (request, response) => {
   Poem.find({}).then((poems) => {
     response.json(poems);
   });
 });
-
-//GET ALL
-// poemsRouter.get("/", (request, response) => {
-//   Poem.find({}).then((poems) => {
-//     response.json(poems);
-//   });
-// });
 
 //GET POEM
 poemsRouter.get("/:id", (request, response, next) => {
@@ -42,8 +36,14 @@ poemsRouter.delete("/:id", (request, response, next) => {
 poemsRouter.post("/", (request, response, next) => {
   const body = request.body;
 
+  if (!body.title || !body.author || !body.text) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
   const poem = new Poem({
-    id: crypto.randomBytes(16).toString("hex"),
+    // id: crypto.randomBytes(16).toString("hex"),
     title: body.title,
     author: body.author,
     text: body.text,
@@ -54,6 +54,31 @@ poemsRouter.post("/", (request, response, next) => {
     .save()
     .then((savedPoem) => {
       response.json(savedPoem);
+    })
+    .catch((error) => next(error));
+});
+
+//update votes
+poemsRouter.post("/:id", (request, response, next) => {
+  const body = request.body;
+
+  if (!body.title || !body.author || !body.text) {
+    console.log("HITTTT !!");
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const update = {
+    votes: request.body.votes,
+    title: request.body.title,
+    text: request.body.text,
+    author: request.body.author,
+  };
+
+  Poem.findByIdAndUpdate(request.params.id, update)
+    .then(() => {
+      response.status(204).end();
     })
     .catch((error) => next(error));
 });
