6e62daf6621cb11c41851ff53711d95b7312e2b0
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-10-01 23:22:52 +1000

    remove comment

--- server/app.js
+++ server/app.js
@@ -7,17 +7,6 @@ const middleware = require("./utils/middleware.js");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 
-// const url = `mongodb+srv://${config.USERNAME}:${config.PASSWORD}@poems.ft4ii.mongodb.net/poems?retryWrites=true&w=majority`;
-// logger.info("connecting to", url);
-// mongoose
-//   .connect(url)
-//   .then(() => {
-//     logger.info("connected to MongoDB");
-//   })
-//   .catch((error) => {
-//     logger.error("error connecting to MongoDB:", error.message);
-//   });
-
 app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
