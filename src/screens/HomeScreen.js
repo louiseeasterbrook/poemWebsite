5c4658266c30e0653c4fbc5aeca6c40ced8f87a3
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-11-22 14:36:29 +1100

    add header section to HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,18 +1,19 @@
 import "./homeScreen.css";
-import TopSplash from "../components/TopSplash";
-
+import image from "../img/feather.png";
+import { useHistory } from "react-router-dom";
 //components
 import PoemItem from "../components/PoemItem";
 import Loading from "../components/Loading";
 
-const HomeScreen = ({ poemData, show }) => {
+const HomeScreen = ({ poemData, showData }) => {
+  let history = useHistory();
   //sort poems in decending order based on no. votes
   let sortedPoems = poemData.sort((a, b) => {
     return b.votes - a.votes;
   });
 
   //conditional rendering
-  if (!show) {
+  if (!showData) {
     return (
       <div className="load-screen">
         <Loading />
@@ -21,20 +22,33 @@ const HomeScreen = ({ poemData, show }) => {
   } else {
     return (
       <div className="homeScreen">
-        <TopSplash />
-        <div className="mainSection">
-          <div className="poemContainer">
-            {sortedPoems.map((el, i) => (
-              <PoemItem
-                key={i}
-                author={el.author}
-                poemTitle={el.title}
-                poemText={el.text}
-                poemId={el.id}
-                votes={el.votes}
-              />
-            ))}
+        <div className="open-header">
+          <div className="open-blurb">
+            <h2>Express yourself with words</h2>
+            <p>Browse and submit your own poems</p>
+            <button onClick={() => history.push("/add")}>
+              Submit your first Poem
+            </button>
           </div>
+          <div className="img-holder">
+            <img
+              src={image}
+              alt="girl on computer - clipart"
+              className="open-img"
+            />
+          </div>
+        </div>
+        <div className="poemContainer">
+          {sortedPoems.map((el, i) => (
+            <PoemItem
+              key={i}
+              author={el.author}
+              poemTitle={el.title}
+              poemText={el.text}
+              poemId={el.id}
+              votes={el.votes}
+            />
+          ))}
         </div>
       </div>
     );
