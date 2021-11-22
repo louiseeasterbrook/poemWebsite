7e0ba42d965bb9559442b82dad41843ad471209c
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-11-22 14:32:46 +1100

    change colour scheme of 'Votes'

--- src/components/Votes.js
+++ src/components/Votes.js
@@ -1,4 +1,6 @@
 import "./votes.css";
+
+//font awesome imports
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
@@ -7,7 +9,7 @@ const PoemItem = (props) => {
     <div className="votesContainer">
       <p>{props.votes}</p>
       <button onClick={() => props.voteAdd()}>
-        <FontAwesomeIcon icon={faArrowUp} />
+        <FontAwesomeIcon icon={faArrowUp} className="up-icon" />
       </button>
     </div>
   );
