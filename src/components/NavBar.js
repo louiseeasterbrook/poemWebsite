c55c0f8b39c7cdba06fb8f3997ba66963d52f313
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-11-22 14:34:08 +1100

    add search bar and sliding menu to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,93 @@
 import "./navBar.css";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { faFeather } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-const NavBar = () => {
+//font awesome imports
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlus,
+  faBars,
+  faHome,
+  faTimes,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
+
+const NavBar = ({ setSearchVal, setSearchPress, searchVal }) => {
+  let history = useHistory();
+  const [sidebar, setSidebar] = useState(false);
+
+  //side bar toggle
+  const showSidebar = () => {
+    setSidebar(!sidebar);
+  };
+
+  //search bar submit function
+  const submitSearch = (e) => {
+    e.preventDefault();
+    //check if earch is blank
+    if (searchVal.trim() !== "") {
+      setSearchPress(true);
+      history.push("/searchResults");
+    }
+  };
+
+  //update search input value
+  const updateSearch = (e) => {
+    setSearchVal(e.target.value);
+  };
+
   return (
-    <div className="navBar">
-      <div className="navBarContainer">
-        <div className="logo">
-          <Link className="pageTitle" to="/">
-            Word Collections
-          </Link>
-          <FontAwesomeIcon icon={faFeather} className="featherIcon" />
+    <div className="nav-container">
+      <div className="top-nav-bar">
+        <div className="top-nav-bar-inner">
+          <div className="title-container">
+            <FontAwesomeIcon
+              icon={sidebar ? faTimes : faBars}
+              className="menu-btn icon"
+              onClick={showSidebar}
+            />
+
+            <Link to="/">
+              <h1 className="long-title">Word Collections</h1>
+              <h1 className="mobile-title">WC</h1>
+            </Link>
+          </div>
+
+          <div className="search-container">
+            <form onSubmit={submitSearch}>
+              <input
+                placeholder="Search.."
+                onChange={updateSearch}
+                value={searchVal}
+              />
+              <button>
+                <FontAwesomeIcon
+                  icon={faSearch}
+                  className="search-icon"
+                  type="submit"
+                />
+              </button>
+            </form>
+          </div>
         </div>
-        <Link className="navBtn-large navBtn" to="/add">
-          Add
-        </Link>
-        <Link className="navBtn-small navBtn" to="/add">
-          <FontAwesomeIcon icon={faPlus} className="plusIcon" />
-        </Link>
       </div>
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-list">
+          <Link to="/">
+            <li className="nav-item">
+              <FontAwesomeIcon icon={faHome} className="menu-icon" />
+              <h3>Home</h3>
+            </li>
+          </Link>
+          <Link to="/add">
+            <li className="nav-item">
+              <FontAwesomeIcon icon={faPlus} className="menu-icon" />
+              <h3>Add Poem</h3>
+            </li>
+          </Link>
+        </ul>
+      </nav>
     </div>
   );
 };
