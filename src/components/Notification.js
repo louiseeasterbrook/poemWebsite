9978e15c852f322309d07ced0de457ccba281de5
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-12-12 01:06:25 +1100

    create Notification Component

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,15 +1,15 @@
 import "./notification.css";
 
-const Notification = ({ message, show }) => {
-  if (show) {
-    return (
-      <div className="notificationContainer">
-        <p>{message}</p>
-      </div>
-    );
-  } else {
-    return <> </>;
-  }
+const Notification = ({ message }) => {
+  // if (show) {
+  return (
+    <div className="notification-container">
+      <p>{message}</p>
+    </div>
+  );
+  // } else {
+  //   return <> </>;
+  // }
 };
 
 export default Notification;
