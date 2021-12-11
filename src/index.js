c4f65598171865222a51fd6e4325dfafd94ccfbb
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-12-12 01:13:03 +1100

    add Auht0 tags to index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-hb2qkwm0.au.auth0.com"
+    clientId="nBYxAyU5UZ2rDBZcIxbGFx1UhYDVdIOp"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
