e3522bd8a4126899912fe49aa0df45f7e32c5217
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-12-12 01:00:29 +1100

    refactor app layout, + modals, - unused screens

--- src/App.js
+++ src/App.js
@@ -1,72 +1,109 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 
 //Screens
 import HomeScreen from "./screens/HomeScreen";
 import PoemScreen from "./screens/PoemScreen";
-import AddScreen from "./screens/AddScreen";
-import UpdateScreen from "./screens/UpdateScreen";
-import ResultScreen from "./screens/ResultScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 //components
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 
+//Modals
+import FormModal from "./components/FormModal";
+import DeleteModal from "./components/DeleteModal";
+import UpdateModal from "./components/UpdateModal";
+
+//auth0 imports
+import { useAuth0 } from "@auth0/auth0-react";
+
 function App() {
   //stores the list of poemss
   const [poems, setPoems] = useState([]);
   // indicates if data has been fetched from the database
   const [showData, setShowData] = useState(false);
-  //stores the searched input value
-  const [searchVal, setSearchVal] = useState("");
-  //if true this variable triggers the search function - used by ResultScreen and NavBar
-  const [searchPress, setSearchPress] = useState(false);
+  //loading data error
+  const [dataError, setDataError] = useState("");
 
-  //GET DATA FROM SERVER
-  useEffect(() => {
-    axios.get("/api/poems").then((response) => {
-      setPoems(response.data);
-      setShowData(true);
-    });
-  }, []);
+  //controls open and close of PoemForm modal
+  const [openModal, setOpenModal] = useState(false);
+  //controls open and close of Delete modal
+  const [openDelModal, setOpenDelModal] = useState(false);
+  //controls open and close of Edit modal
+  const [openEditModal, setOpenEditModal] = useState(false);
+  const [selectedPoem, setSelectedPoem] = useState({});
+
+  //data for user profile
+  const [profilePoems, setProfilePoems] = useState([]);
+
+  //for login authentication
+  const { user, isAuthenticated } = useAuth0();
 
   return (
     <Router>
       <NavBar
-        setSearchVal={setSearchVal}
-        searchVal={searchVal}
-        setSearchPress={setSearchPress}
+        isAuthenticated={isAuthenticated}
+        user={user}
+        setOpenModal={setOpenModal}
       />
 
       <main>
         <ScrollToTop />
         <Switch>
           <Route exact path="/">
-            <HomeScreen poemData={poems} showData={showData} />
+            <HomeScreen
+              allPoems={poems}
+              setAllPoems={setPoems}
+              showData={showData}
+              setShowData={setShowData}
+              dataError={dataError}
+              setDataError={setDataError}
+              setOpenModal={setOpenModal}
+              isAuthenticated={isAuthenticated}
+            />
           </Route>
           <Route exact path="/poem/:id">
             <PoemScreen setPoems={setPoems} poems={poems} />
           </Route>
-          <Route exact path="/add">
-            <AddScreen setPoems={setPoems} poemData={poems} />
-          </Route>
-          <Route exact path="/update/:id">
-            <UpdateScreen setPoems={setPoems} poemData={poems} />
-          </Route>
-          <Route exact path="/searchresults">
-            <ResultScreen
-              poemData={poems}
-              searchVal={searchVal}
-              setSearchVal={setSearchVal}
-              searchPress={searchPress}
-              setSearchPress={setSearchPress}
+          <Route exact path="/profile">
+            <ProfileScreen
+              user={user}
+              isAuthenticated={isAuthenticated}
+              setSelectedPoem={setSelectedPoem}
+              setOpenDelModal={setOpenDelModal}
+              setOpenEditModal={setOpenEditModal}
+              profilePoems={profilePoems}
+              setProfilePoems={setProfilePoems}
             />
           </Route>
         </Switch>
       </main>
+      <FormModal
+        open={openModal}
+        setOpenModal={setOpenModal}
+        setPoems={setPoems}
+        poemData={poems}
+        user={user}
+        allPoems={poems}
+        setAllPoems={setPoems}
+      />
+
+      <DeleteModal
+        open={openDelModal}
+        setOpenModal={setOpenDelModal}
+        selectedPoem={selectedPoem}
+        profilePoems={profilePoems}
+        setProfilePoems={setProfilePoems}
+      />
+      <UpdateModal
+        open={openEditModal}
+        setOpenModal={setOpenEditModal}
+        user={user}
+        selectedPoem={selectedPoem}
+      />
       <Footer />
     </Router>
   );
